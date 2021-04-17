72ced568d3ce4cd98888a2414435c09471e53e76
Moritz Ulmer <moritz.ulmer@posteo.de>
2021-04-17 17:42:04 +0200

    Update controller

--- hello.py
+++ hello.py
@@ -1,6 +1,9 @@
 import os
 import sys
 
+### Settings
+ui_file_name = "mainwindow.ui"
+
 # Needed for Wayland applications
 os.environ["QT_QPA_PLATFORM"] = "xcb"
 # Change the current dir to the temporary one created by PyInstaller
@@ -11,23 +14,27 @@ except:
     pass
 
 from PySide2.QtUiTools import QUiLoader
-from PySide2.QtWidgets import QApplication
+from PySide2.QtWidgets import QApplication, QMainWindow
 from PySide2.QtCore import QFile, QIODevice
 
+class DSFlasherUi(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        ui_file = QFile(ui_file_name)
+        if not ui_file.open(QIODevice.ReadOnly):
+            print(f"Cannot open {ui_file_name}: {ui_file.errorString()}")
+            sys.exit(-1)
+        self.ui = QUiLoader().load(ui_file)
+        ui_file.close()
+    
+    def show(self):
+        self.ui.show()
+
+def main():
+    ds_flasher = QApplication(sys.argv)
+    view = DSFlasherUi()
+    view.show()
+    sys.exit(ds_flasher.exec_())
+
 if __name__ == "__main__":
-    app = QApplication(sys.argv)
-
-    ui_file_name = "mainwindow.ui"
-    ui_file = QFile(ui_file_name)
-    if not ui_file.open(QIODevice.ReadOnly):
-        print(f"Cannot open {ui_file_name}: {ui_file.errorString()}")
-        sys.exit(-1)
-    loader = QUiLoader()
-    window = loader.load(ui_file)
-    ui_file.close()
-    if not window:
-        print(loader.errorString())
-        sys.exit(-1)
-    window.show()
-
-    sys.exit(app.exec_())
+    main()
