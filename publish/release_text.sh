1cb621faef3b118b0fc60cd2084fa38a5c23565a
Moritz Ulmer <moritz.ulmer@posteo.de>
2023-09-03 19:38:38 +0200

    Update docs

--- publish/release_text.sh
+++ publish/release_text.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 LAST_TAG=$(git describe HEAD~ --abbrev=0 --tags)
-git log --pretty=format:'### %s%n%n%b' "$LAST_TAG"..HEAD
+# shellcheck disable=SC2005
+echo "$(git log --pretty=format:'### %s%n%n%b' "$LAST_TAG"..HEAD)"
